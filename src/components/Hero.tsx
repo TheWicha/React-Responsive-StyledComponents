d89218e740846af18e5cfb7fd93494178f7ae65d
TheWicha <kontakt.danielwisniewski@gmail.com>
2020-06-09 19:36:47 +0200

    Create Hero unit grid

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,61 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import styled from "styled-components";
 
-const StyledHero = styled.div`
-    height: 100vh;
-    background-color: #23C9FF;
-    margin-top: 20px;
-`
+const Hero = () => {
+  return (
+    <StyledHero>
+      <StyledRow>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>1</Item>
+        </StyledCol>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>2</Item>
+        </StyledCol>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>3</Item>
+        </StyledCol>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>1</Item>
+        </StyledCol>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>2</Item>
+        </StyledCol>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>3</Item>
+        </StyledCol>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>1</Item>
+        </StyledCol>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>2</Item>
+        </StyledCol>
+        <StyledCol xs={12} md={6} lg={4}>
+          <Item>3</Item>
+        </StyledCol>
+      </StyledRow>
+    </StyledHero>
+  );
+};
 
-const Hero: React.FC = () => {
-    return(
-        <StyledHero>
-            ..i..
-        </StyledHero>
+const StyledHero = styled(Container)`
+  height: 100%;
+  background-color: #23c9ff;
+  margin-top: 20px;
+  padding: 40px;
+`;
 
-    )
-}
+const StyledRow = styled(Row)`
+  margin: 0px;
+`;
 
-export default Hero
+const StyledCol = styled(Col)`
+  margin-top: 15px;
+`;
+const Item = styled.div`
+  border: 1px solid black;
+  margin: 0 auto;
+  min-height: 300px;
+  max-width: 250px;
+`;
+export default Hero;
